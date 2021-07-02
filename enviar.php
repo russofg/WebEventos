7f4663a4fc673292f3792ac4394a7519ddc68d7e
fernandorusso <russofg@gmail.com>
2021-07-02 20:49:57 -0300

    cambios en archivos php de mail

--- enviar.php
+++ enviar.php
@@ -1,26 +1,22 @@
 <?php
 $nombre - $_POST['nombre'];
 $mail - $_POST['email'];
-$consula - $_POST['consulta'];
+$consulta - $_POST['consulta'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
-​
+
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['consulta'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
-​
+
 $para = 'russofg@gmail.com'; // El mail a donde van a llegar los mensajes
-$asunto = 'Consulta de Plataforma Eventos';
-​
+$asunto = 'Mensaje de mi sitio web';
+
 mail($para, $asunto, utf8_decode($mensaje), $header);
-​
+
 header('Location:index.html');
-​
-
-
-
 ?>
